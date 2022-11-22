77b7948f67c23b81a5d17e4c8af4c753d8b92a36
Thái <62926491+nhthai173@users.noreply.github.com>
2022-11-22 10:44:26 +0700

    Prettify

--- api.js
+++ api.js
@@ -1,16 +1,17 @@
 //https://script.google.com/macros/s/AKfycbyCbVnx0UhAt25F2K4RcRf9znOCUW0aUI2VmRyOlQ/exec
 
 function doGet(e) {
-  function stripQuotes( value ) {
+  function stripQuotes(value) {
     return value.replace(/^["']|['"]$/g, "");
   }
   var result = 'Ok'
   if (e.parameter == 'undefined') {
     result = 'No Parameters';
-  }else {
+  } else {
     for (var param in e.parameter) {
-      var value = stripQuotes(e.parameter[param]);
+      var value = stripQuotes(e.parameter[ param ]);
       switch (param) {
+        /* Fetch new by DVVC */
         case 'jt':
           jt()
           break;
@@ -23,18 +24,21 @@ function doGet(e) {
         case 'bestexpress': case 'ninjavan': case 'vnpost': case 'viettelpost': case 'spx':
           tracktry(param)
           break;
-
+        /* Check if valid code */
         case 'check':
           var r = JSON.parse(value)
           result = JSON.stringify(main(r.dvvc, r.mavandon))
           break
+        /* Add */
         case 'add':
           var d = JSON.parse(value)
           addVD(d.id, d.dvvc, d.mavandon, d.tendonhang, d.interval)
           break
+        /* Get All */
         case 'getAll':
           getAll(value)
           break
+        /* Delete */
         case 'delete':
           var d = JSON.parse(value)
           result = deleteVD(d.dvvc, d.mavandon)
